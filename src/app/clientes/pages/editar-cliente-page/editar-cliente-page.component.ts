8beeda39abee5958fe222edf5855164746915629
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2025-09-03 13:53:06 -0600

    se crea componente para editar el cliente

---
+++ src/app/clientes/pages/editar-cliente-page/editar-cliente-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-cliente-page',
+  templateUrl: './editar-cliente-page.component.html'
+})
+export class EditarClientePageComponent {
+
+}
