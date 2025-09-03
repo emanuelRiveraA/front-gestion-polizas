bf46fde63abd06b592340e110a0affee415601b7
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2025-09-03 13:03:27 -0600

    importacion de componentes

--- src/app/clientes/clientes.module.ts
+++ src/app/clientes/clientes.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ClientesPageComponent } from './pages/clientes-page/clientes-page.component';
+import { RegistrarClientePageComponent } from './pages/registrar-cliente-page/registrar-cliente-page.component';
+import { EditarClientePageComponent } from './pages/editar-cliente-page/editar-cliente-page.component';
+import { ClientesRoutingModule } from './clientes-routing.module';
+import { ClienteTableComponent } from './components/cliente-table/cliente-table.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ClientesPageComponent,
+    RegistrarClientePageComponent,
+    EditarClientePageComponent,
+    ClienteTableComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    ClientesRoutingModule,
+    ReactiveFormsModule
+    
   ]
 })
 export class ClientesModule { }
