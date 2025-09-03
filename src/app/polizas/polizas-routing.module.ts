3ffea062c34717c08bedc8e5e68f72b04b711d27
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2025-09-03 16:21:39 -0600

    se crean las rutas para modulo poliza

---
+++ src/app/polizas/polizas-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PolizasPageComponent } from './pages/polizas-page/polizas-page.component';
+
+const routes: Routes = [
+    {
+        path: 'polizas',
+        component: PolizasPageComponent
+    },
+    {
+        path: 'by/:id',
+        component: PolizasPageComponent
+    },
+    {
+        path: '**',
+        redirectTo: "polizas"
+    }
+]
+
+@NgModule({
+    imports: [
+        RouterModule.forChild(routes),
+    ],
+    exports: [
+        RouterModule
+    ],
+})
+export class PolizasRoutingModule { }
