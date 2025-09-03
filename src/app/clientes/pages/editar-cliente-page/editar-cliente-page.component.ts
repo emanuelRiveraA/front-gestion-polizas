4b33ce2ccd7708d0aea7d9df6f23dc39ceed1c48
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2025-09-03 14:30:54 -0600

    formulario para editar cliente

--- src/app/clientes/pages/editar-cliente-page/editar-cliente-page.component.ts
+++ src/app/clientes/pages/editar-cliente-page/editar-cliente-page.component.ts
@@ -1,9 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ClientesService } from '../../services/clientes.service';
+import { Cliente } from '../../interfaces/Cliente.interface';
 
 @Component({
   selector: 'app-editar-cliente-page',
   templateUrl: './editar-cliente-page.component.html'
 })
-export class EditarClientePageComponent {
+export class EditarClientePageComponent implements OnInit{
 
+    registroForm!: FormGroup;
+  
+    constructor(
+      private fb: FormBuilder,
+      private clienteService: ClientesService
+    ){}
+  
+     ngOnInit() {
+      this.registroForm = this.fb.group({
+        numeroIdentificacion: ['', Validators.required],
+        nombreCompleto: ['', Validators.required],
+        correo: ['', [Validators.required, Validators.email]],
+        telefono: ['', [Validators.required, Validators.pattern(/^[0-9]{7,15}$/)]],
+        direccion: ['']
+      });
+    }
+  
+    onSubmit() {
+      if (this.registroForm.valid) {
+        const cliente: Cliente = this.registroForm.value;
+        console.log('Datos del formulario:', cliente);
+        // Aquí se llama al servicio para registrar cliente
+        this.clienteService.registrarCliente(cliente)
+          .subscribe({
+            next: (response) => {
+              if (response && response.id) {
+                console.log("Registro exitoso:", response);
+                // Optionally reset form or navigate
+              } else {
+                console.error("Registro fallido: respuesta inválida");
+              }
+            },
+            error: (err) => {
+              console.error("Error al registrar cliente:", err);
+            }
+          });
+      } else {
+        console.log('Formulario inválido');
+        this.registroForm.markAllAsTouched(); // Para mostrar errores
+      }
+    }
 }
