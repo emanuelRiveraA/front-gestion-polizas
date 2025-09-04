80d55eefc161bba6dc3792feddd1641d4e30843c
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2025-09-03 23:55:10 -0600

    se elimina componente

--- src/app/polizas/pages/editar-poliza-page/editar-poliza-page.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-editar-poliza-page',
-  templateUrl: './editar-poliza-page.component.html'
-})
-export class EditarPolizaPageComponent {
-
-}
