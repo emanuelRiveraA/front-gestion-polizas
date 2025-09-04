d7dee127b82275e05455c86e4905e9815e0edf19
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2025-09-03 23:55:49 -0600

    se importan componentes y modulos

--- src/app/polizas/polizas.module.ts
+++ src/app/polizas/polizas.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PolizasPageComponent } from './pages/polizas-page/polizas-page.component';
 import { RegistrarPolizaPageComponent } from './pages/registrar-poliza-page/registrar-poliza-page.component';
-import { EditarPolizaPageComponent } from './pages/editar-poliza-page/editar-poliza-page.component';
 import { PolizasRoutingModule } from './polizas-routing.module';
+import { PolizaTableComponent } from './components/poliza-table/poliza-table.component';
+import { SharedModule } from '../shared/shared.module';
+import { AsociarPolizaPageComponent } from './pages/asociar-poliza-page/asociar-poliza-page.component';
+import { ClientesModule } from '../clientes/clientes.module';
+import { ɵInternalFormsSharedModule } from "@angular/forms";
 
 
 
@@ -11,11 +15,15 @@ import { PolizasRoutingModule } from './polizas-routing.module';
   declarations: [
     PolizasPageComponent,
     RegistrarPolizaPageComponent,
-    EditarPolizaPageComponent
+    PolizaTableComponent,
+    AsociarPolizaPageComponent
   ],
   imports: [
     CommonModule,
-    PolizasRoutingModule
-  ]
+    PolizasRoutingModule,
+    SharedModule,
+    ClientesModule,
+    ɵInternalFormsSharedModule
+]
 })
 export class PolizasModule { }
